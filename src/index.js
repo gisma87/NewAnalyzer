1a5fca235ddd19a1d3937cec158251f4651a59e4
stivman <gisma87@yandex.ru>
2020-06-24 13:46:47 +0700

    compress image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/style.css';
 
+// временно для проверки сборки
 const numbers = [2, 3, 5];
-const doubledNumbers = numbers.map(number => number * 2); // Стрелочная функция. Не запнётся ли на ней Internet Explorer
-
+const doubledNumbers = numbers.map(number => number * 2);
 console.log(doubledNumbers); // 4, 6, 10
